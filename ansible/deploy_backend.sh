84404e2aa21f6252f45ffdac87e882cd98685111
zzyang1994 <yangzhanzhao1994@gmail.com>
2022-05-10 04:19:14 +1000

    feat: adjust ansible

--- ansible/deploy_backend.sh
+++ ansible/deploy_backend.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ansible-playbook -i ./inventory/openstack_inventory.py -u ubuntu --key-file=~/.ssh/test01privatekey deploy_frontend.yaml
+ansible-playbook -i ./inventory/openstack_inventory.py -u ubuntu --key-file=~/.ssh/test01privatekey deploy_backend.yaml
